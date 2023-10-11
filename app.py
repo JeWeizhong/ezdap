f65605a00900f2b8de780ee01d19ceedda2c5704
JeWeizhong <zhmqsnq90@foxmail.com>
2023-10-11 17:29:54 +0800

    add dialog

--- app.py
+++ app.py
@@ -7,11 +7,12 @@ from PySide6.QtGui import QAction, QIcon, QKeySequence, QGuiApplication
 
 from modules.dataframe_model import PandasModel
 from modules.mpl_plot import PlotWidget
-from modules.form_widget import FormWidget
+# from modules.form_widget import FormWidget, FormDialog
 # from modules.menu_module import MainMenu, FileSlot, PlotSlot
-
-from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
+from matplotlib.backends.backend_qtagg import FigureCanvas
+from matplotlib.backends.backend_qtagg import NavigationToolbar2QT
+# from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
 
 
 class AppMainWindow(QMainWindow):
@@ -35,6 +36,7 @@ class AppMainWindow(QMainWindow):
         if screen_resolution.width()>1024:
             self.setGeometry(QRect(200, 200, width, height))
         self.setMinimumSize(400,300)
+        self.setLayout(layout)
         self.main.setFocus()
         self.setCentralWidget(self.main)
 
@@ -48,9 +50,10 @@ class AppMainWindow(QMainWindow):
         view.setModel(self.data_model)
         self.splitter = QSplitter(Qt.Horizontal)
         self.splitter.addWidget(view)
-        # form = FormWidget(data_frame=model._dataframe, app_name='scatter')
-        form = FormWidget()
-        self.right_form = form.grid_group_box
+        layout = QFormLayout()
+        grid_group_box = QGroupBox("Plot")
+        grid_group_box.setLayout(layout)
+        self.right_form = grid_group_box
         self.splitter.addWidget(self.right_form)
         self.splitter.setSizes((500,200))
         # self.current_table = None
@@ -114,24 +117,52 @@ class AppMainWindow(QMainWindow):
         # plot_action = PlotAction(self)
         plot_menu = menubar.addMenu("可视化")
         scatter_action = QAction("散点图", menubar)
-        scatter_action.triggered.connect(self.create_scatter_dock)
+        scatter_action.triggered.connect(self.create_plot_dialog)
         plot_menu.addAction(scatter_action)
         
         help_menu = menubar.addMenu("帮助")
 
         self.setMenuBar(menubar)
 
-    def create_scatter_dock(self):
-        # 检查 current_table 是否存在
-        if not hasattr(self, "data_model"):
-            print("请先打开一个表格")
-            return
-        form = FormWidget(data_frame=self.data_model._dataframe, app_name='scatter')
-        # self.splitter.removeWidget(self.right_form)
-        self.right_form = form.grid_group_box
-        self.splitter.replaceWidget(1, self.right_form)
-        return
+    # def create_plot_dock(self):
+    #     # 检查 current_table 是否存在
+    #     if not hasattr(self, "data_model"):
+    #         print("请先打开一个表格")
+    #         return
+    #     # form = FormWidget(data_frame=self.data_model._dataframe, app_name='scatter')
+    #     # self.splitter.removeWidget(self.right_form)
+    #     grid_group_box = QGroupBox("Grid layout")
+    #     self.right_form = grid_group_box
+    #     self.splitter.replaceWidget(1, self.right_form)
+    #     return
 
+    def create_plot_dialog(self):
+        
+        dialog = QDialog()
+        dialog.setWindowTitle("散点图")
+        dialog.setMinimumWidth(250)
+        dialog.setWindowModality(Qt.WindowModal)
+        layout = QFormLayout()
+        xaixs = QComboBox()
+        xaixs.addItems(self.data_model._dataframe.columns.tolist())
+
+        layout.addRow(QLabel("x 轴"), xaixs)
+        yaixs = QComboBox()
+        yaixs.addItems(self.data_model._dataframe.columns.tolist())
+        layout.addRow(QLabel("y 轴"), yaixs)
+
+        h_layout = QHBoxLayout() 
+        submit_button = QPushButton("开始运行", dialog)
+        submit_button.clicked.connect(self.mpl_plot_scatter)
+        reset_button = QPushButton("Reset")
+        h_layout.addWidget(submit_button)
+        h_layout.addWidget(reset_button)
+        
+        layout.addRow(h_layout)
+        dialog.setLayout(layout)
+        
+        if dialog.exec() == QDialog.Accepted:
+            print(f"执行成功")
 
     def new_document(self):
         """
@@ -156,6 +187,26 @@ class AppMainWindow(QMainWindow):
 
     def close(self) -> bool:
         return super().close()
+    
+    def mpl_plot_scatter(self):
+        print("开始绘制散点图")
+        dockWidget = QDockWidget("可关闭/右侧栏", self)
+        # layout = QVBoxLayout()
+        # view = FigureCanvas(Figure(figsize=(5, 3)))
+        # axes = view.figure.subplots()
+        # axes.plot([0,1,2,3,4], [10,1,20,3,40])
+        # toolbar = NavigationToolbar2QT(view)
+        # layout.addWidget(toolbar)
+        # layout.addWidget(view)
+        listWidget = QListWidget()
+        listWidget.addItem(f"dock1-item1")
+        listWidget.addItem(f"dock2-item2")
+        listWidget.addItem(f"dock3-item3")
+        dockWidget.setWidget(listWidget)
+        dockWidget.setFeatures(QDockWidget.DockWidgetClosable | QDockWidget.DockWidgetMovable)
+
+        self.addDockWidget(Qt.RightDockWidgetArea, dockWidget)
+        # view.draw()
 
 if __name__ == "__main__":
 
--- modules/form_widget.py
+++ modules/form_widget.py
@@ -2,13 +2,14 @@
 可视化菜单
 '''
 
-from PySide6.QtWidgets import QMenu, QToolBar, QWidget, QDockWidget, QComboBox, QFormLayout, QGroupBox, QLabel, QPushButton, QHBoxLayout
+from PySide6.QtWidgets import QMenu, QToolBar, QWidget, QDockWidget, QComboBox, QFormLayout, QGroupBox, QLabel, QPushButton, QHBoxLayout, QDialog, QVBoxLayout, QLineEdit
 from PySide6.QtCore import QAbstractTableModel, Qt, QModelIndex, QSize
 from PySide6.QtGui import QAction, QIcon, QKeySequence
 
-class FormWidget:
+class FormWidget(QWidget):
 
     def __init__(self, title="Grid layout", data_frame=None, app_name=None):
+        super().__init__()
         self.title = title
         self.data_frame = data_frame
         self.layout = QFormLayout()
@@ -19,6 +20,10 @@ class FormWidget:
             self.grid_group_box = self.create_scatter(self.data_frame)
     
     def create_scatter(self, data_frame):
+        submit_button = QPushButton()
+        submit_button.setText("对话框5-模式窗口2(exec)")
+        # layout.addWidget(self.submit_button)
+        submit_button.clicked.connect(self.showdialog_model2)
         grid_group_box = QGroupBox("Grid layout")
         layout = QFormLayout()
         xaixs = QComboBox()
@@ -31,7 +36,7 @@ class FormWidget:
         # 提交按钮, 重置按钮
         # 水平布局
         h_layout = QHBoxLayout() 
-        submit_button = QPushButton("Submit")
+        # submit_button = QPushButton("Submit")
         reset_button = QPushButton("Reset")
         h_layout.addWidget(submit_button)
         h_layout.addWidget(reset_button)
@@ -39,3 +44,34 @@ class FormWidget:
         grid_group_box.setLayout(layout)
 
         return grid_group_box
+    
+    
+    def showdialog_model2(self):
+        dialog = QDialog(self)
+        button = QPushButton("OK", dialog)
+        button.clicked.connect(dialog.accept)
+        dialog.setWindowTitle("Dialog 案例-模式窗口2(exec)")
+        dialog.setMinimumWidth(250)
+        dialog.setWindowModality(Qt.WindowModal)
+        self.label.setText('修改默认模式：“%s” \n 我有父类，我能影响父窗口，不能影响兄弟窗口' % dialog.windowTitle())
+        dialog.exec()
+
+class FormDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("表单对话框")
+
+        layout = QVBoxLayout()
+
+        label = QLabel("请输入一些文本:")
+        self.text_field = QLineEdit()
+        ok_button = QPushButton("确定")
+
+        layout.addWidget(label)
+        layout.addWidget(self.text_field)
+        layout.addWidget(ok_button)
+
+        ok_button.clicked.connect(self.accept)
+
+        self.setLayout(layout)
